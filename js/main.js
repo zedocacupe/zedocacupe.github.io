88820cd2f66e1d6ee00018a45af18df7a39017c8
zedocacupe <78446750+zedocacupe@users.noreply.github.com>
2021-11-12 11:27:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
     //var dia_atual = hoje.getDate()+"/"+mes+"/"+hoje.getFullYear();
     var dia_atual = hoje.getFullYear()+"-"+mes+"-"+hoje.getDate();
 
-    var horario_carnaval = true;//Date.parse(dia_atual+'T'+hora_atual) <= Date.parse('2021-11-03T00:00:00');
+    var horario_carnaval = Date.parse(dia_atual+'T'+hora_atual) <= Date.parse('2021-11-16T00:00:00');
 
     if(dia_semana == Sunday){
       if(horario_carnaval){
@@ -65,7 +65,7 @@ $(document).ready(function(){
     }
 
     if(dia_semana == Monday){
-      if(Date.parse('01/01/2011 '+hora_atual) >= Date.parse('01/01/2011 11:30:00') && Date.parse('01/01/2011 '+hora_atual) <= Date.parse('01/01/2011 16:00:00')){
+      if(Date.parse('01/01/2011 '+hora_atual) >= Date.parse('01/01/2011 11:30:00') && Date.parse('01/01/2011 '+hora_atual) <= Date.parse('01/01/2011 17:00:00')){
       	aberto = true;
       }
       if(horario_carnaval){
@@ -79,9 +79,10 @@ $(document).ready(function(){
 
     if(dia_semana == Tuesday){
       if(horario_carnaval){
-        if(Date.parse('01/01/2011 '+hora_atual) >= Date.parse('01/01/2011 11:30:00') && Date.parse('01/01/2011 '+hora_atual) <= Date.parse('01/01/2011 17:30:00')){
-          aberto = true;
-        }
+        aberto = false;
+        //if(Date.parse('01/01/2011 '+hora_atual) >= Date.parse('01/01/2011 11:30:00') && Date.parse('01/01/2011 '+hora_atual) <= Date.parse('01/01/2011 17:30:00')){
+        //  aberto = true;
+        //}
       } else {
         if(Date.parse('01/01/2011 '+hora_atual) >= Date.parse('01/01/2011 11:30:00') && Date.parse('01/01/2011 '+hora_atual) <= Date.parse('01/01/2011 23:00:00')){
           aberto = true;
